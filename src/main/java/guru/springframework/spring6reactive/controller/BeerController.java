62a55f40ecb9b46cb4754a26091e23ec090ca9ac
Lydia Rennert <lydia.rennert@gmail.com>
2025-06-17 10:33:29 +0200

    lre: Add endpoint for getBeerById

--- src/main/java/guru/springframework/spring6reactive/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6reactive/controller/BeerController.java
@@ -4,19 +4,27 @@ import guru.springframework.spring6reactive.model.BeerDTO;
 import guru.springframework.spring6reactive.service.BeerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
 public class BeerController {
 
     public static final String BEER_PATH = "/api/v2/beer";
+    public static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
 
     private final BeerService beerService;
 
     @GetMapping(BEER_PATH)
-    Flux<BeerDTO> listBeers(){
+    Flux<BeerDTO> listBeers() {
         return beerService.listBeers();
+    }
+
+    @GetMapping(BEER_PATH_ID)
+    Mono<BeerDTO> getBeerById(@PathVariable("beerId") Integer beerId) {
+        return beerService.getBeerById(beerId);
     }
 }
--- src/main/java/guru/springframework/spring6reactive/service/BeerService.java
+++ src/main/java/guru/springframework/spring6reactive/service/BeerService.java
@@ -2,8 +2,11 @@ package guru.springframework.spring6reactive.service;
 
 import guru.springframework.spring6reactive.model.BeerDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface BeerService {
 
     Flux<BeerDTO> listBeers();
+
+    Mono<BeerDTO> getBeerById(Integer beerId);
 }
