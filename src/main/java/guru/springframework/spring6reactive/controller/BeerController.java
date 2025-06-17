6106676a8d15bb89d8f1889a765cbfea765691e9
Lydia Rennert <lydia.rennert@gmail.com>
2025-06-17 15:33:32 +0200

    lre: Add endpoint for upateBeer

--- src/main/java/guru/springframework/spring6reactive/controller/BeerController.java
+++ src/main/java/guru/springframework/spring6reactive/controller/BeerController.java
@@ -5,8 +5,10 @@ import guru.springframework.spring6reactive.service.BeerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -42,4 +44,20 @@ public class BeerController {
                                 .build().toUri())
                         .build());
     }
+
+    @PutMapping(BEER_PATH_ID)
+    Mono<ResponseEntity<Void>> updateExistingBeer(@PathVariable("beerId") Integer beerId,
+                                                  @RequestBody BeerDTO beerDTO) {
+        return beerService.updateBeer(beerId, beerDTO)
+                .map(_ -> ResponseEntity.noContent().build());
+    }
+
+    /* Option with backpressure in the controller, anti-pattern in WebFlux
+    @PutMapping(BEER_PATH_ID)
+    ResponseEntity<Void> updateExistingBeer(@PathVariable("beerId") Integer beerId,
+                                            @RequestBody BeerDTO beerDTO) {
+        beerService.updateBeer(beerId, beerDTO).subscribe();
+        return ResponseEntity.ok().build();
+    }
+    */
 }
