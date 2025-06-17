a76d749af88ac7edbfaedffa828acee8e775efea
Lydia Rennert <lydia.rennert@gmail.com>
2025-06-17 08:57:41 +0200

    lre: Assignment 27

---
+++ src/main/java/guru/springframework/spring6reactive/mapper/BeerMapper.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6reactive.mapper;
+
+import guru.springframework.spring6reactive.domain.Beer;
+import guru.springframework.spring6reactive.model.BeerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+
+    Beer beerDtoToBeer(BeerDTO dto);
+    BeerDTO beerToBeerDto(Beer beer);
+}
